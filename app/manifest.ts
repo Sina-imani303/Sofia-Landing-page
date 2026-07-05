72474e979c8844a8797bb90df1287525eb623588
Sina <sinwsep@gmail.com>
2026-07-05 11:29:34 +0330

    icon problem

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,21 +4,25 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Sofia",
     short_name: "Sofia",
-    description: "Sofia Software Company",
+    description:
+      "Sofia helps you manage your projects, services, and collaboration in one place.",
     start_url: "/",
+    scope: "/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#14B8A6",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
       },
     ],
   };
